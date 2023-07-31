fce2fa1efcc24046a61c866b54b85172569e3d01
hassaanarshad228 <hassanarshad228@gmail.com>
2023-07-31 10:05:48 +0500

    My Student Web Site

---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebForms_WSDLTest.Models
+{
+    public class Student
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int TotalMarks { get; set; }
+
+    }
+}
---
+++ StudentService.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using WebForms_WSDLTest.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebForms_WSDLTest
+{
+    /// <summary>
+    /// Summary description for StudentService
+    /// </summary>
+    [WebService(Namespace = "http://pragimtech.com/webservices")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
+    public class StudentService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public Student GetStudentByID(int ID)
+        {
+            Student student = new Student(); ;
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetStudentByID", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter parameter = new SqlParameter("@ID", ID);
+                cmd.Parameters.Add(parameter);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    student.ID = Convert.ToInt32(reader["ID"]);
+                    student.Name = reader["Name"].ToString();
+                    student.Gender = reader["Gender"].ToString();
+                    student.TotalMarks = Convert.ToInt32(reader["TotalMarks"]);
+                }
+            }
+            return student;
+        }
+    }
+}
